3b48a5ff95e59cd9ca0d0c4b6d9154d55f1ad96e
rafael1337 <rafael_aldo_s@hotmail.com>
2014-04-30 11:53:57 -0300

    Operadores

--- RafaelRepositorio/Exercicio6/ExercicioSlide/Exercicio1DoSlide_OperadoresAritimeticos.cs
+++ RafaelRepositorio/Exercicio6/ExercicioSlide/Exercicio1DoSlide_OperadoresAritimeticos.cs
@@ -8,5 +8,25 @@ namespace Unidade6.ExercicioSlide
 {
     class Exercicio1DoSlide_OperadoresAritimeticos
     {
+        static void Main(string[] args)
+        {
+           int x,y,z;
+           x = 7;
+           y = 7;
+                
+          z = x + y;
+          z = x - y;
+          z = x / y;
+          z = x * y;
+
+          Math.Pow(x, y);
+          Math.Sqrt(y);
+          z =  x % y ;
+
+
+            Console.WriteLine(1 + 2 + 3 + "AHAHAHAHA");
+            Console.WriteLine("AHAHAHAHA" + 1 + 2 + 3);
+            double a = (double) 5 / 2;
+        }
     }
 }
