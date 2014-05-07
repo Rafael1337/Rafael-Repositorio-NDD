bf68b282d7493b00ba6f43a8c36a75742f951e1f
rafael1337 <rafael_aldo_s@hotmail.com>
2014-05-07 09:26:33 -0300

    atualizado

--- RafaelRepositorio/Exercicio6/ExercicioSlide/Exercicio1DoSlide_OperadoresAritimeticos.cs
+++ RafaelRepositorio/Exercicio6/ExercicioSlide/Exercicio1DoSlide_OperadoresAritimeticos.cs
@@ -8,6 +8,7 @@ namespace Unidade6.ExercicioSlide
 {
     class Exercicio1DoSlide_OperadoresAritimeticos
     {
+        public static double a;
         static void Main111(string[] args)
         {
            int x,y,z;
@@ -23,10 +24,10 @@ namespace Unidade6.ExercicioSlide
           Math.Sqrt(y);
           z =  x % y ;
 
-
+         
             Console.WriteLine(1 + 2 + 3 + "AHAHAHAHA");
             Console.WriteLine("AHAHAHAHA" + 1 + 2 + 3);
-            double a = (double) 5 / 2;
+             a = (double) 5 / 2;
 
             int valor = 1;
             valor += 2;
