188e7641efffb9907173fd0c6799bba6c8317f0c
rafael1337 <rafael_aldo_s@hotmail.com>
2014-05-15 10:02:40 -0300

    atualizado

--- RafaelRepositorio/Unidade8/Complementares/2.cs
+++ RafaelRepositorio/Unidade8/Complementares/2.cs
@@ -8,8 +8,8 @@ namespace Unidade8.Complementares
 {
     class _2
     {
-        public static string tamanho,tamanho2,tamanho3;
         public static int i, aux, g;
+        public static string tamanho, tamanho2, tamanho3;
         public static void partebranca(int a)
         {
             for (i = 0; i < a; i++)
@@ -35,14 +35,13 @@ namespace Unidade8.Complementares
             }
 
         }
-        static void Main(string[] args)
+        static void Main11(string[] args)
         {         
             tamanho = "*******";
                 tamanho2 = "   *******";
                 tamanho3 = "       *******";
                 for (int h = 0; h < 1;h++ )
                 {
-                    {
                         for (g = 1; g <= 5; g++)
                         {
                             partebranca(aux);
@@ -61,7 +60,7 @@ namespace Unidade8.Complementares
                             Console.WriteLine(tamanho3);
                             aux++;
                         }                    
-                    }
+                    
                 }
         }
     }
---
+++ RafaelRepositorio/Unidade8/Complementares/3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidade8.Complementares
+{
+    class _3
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
