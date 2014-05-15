ab65d8b3ee15d3fad81893b4302df381bba372a0
rafael1337 <rafael_aldo_s@hotmail.com>
2014-05-15 08:58:38 -0300

    2 done

--- RafaelRepositorio/Unidade8/Complementares/2.cs
+++ RafaelRepositorio/Unidade8/Complementares/2.cs
@@ -8,12 +8,67 @@ namespace Unidade8.Complementares
 {
     class _2
     {
+        public static string tamanho,tamanho2,tamanho3;
+        public static int i, aux, g;
+        public static void partebranca(int a)
+        {
+            for (i = 0; i < a; i++)
+            {
+                Console.Write(" ");
+            }
+
+        }
+        public static void partebranca2(int b)
+        {
+            for (i = 0; i < b; i++)
+            { 
+                Console.Write(" ");
+            }
+
+        }
+
+        public static void partebranca3(int c)
+        {
+            for (i = 0; i < c; i++)
+            {
+                Console.Write(" ");
+            }
+
+        }
         static void Main(string[] args)
         {
-            for (int i = 0; i <= 15; i++)
+            aux = 0;
+            i = 0;
+            tamanho = "*******";
+                tamanho2 = "   *******";
+                tamanho3 = "       *******";
+            while (i < 2)
             {
-                Console.WriteLine(" ********** \t");
+                {
+                    for (g = 0; g <= 5; g++)
+                    {
+                        partebranca(aux);
+                        Console.WriteLine(tamanho);
+                        aux++;
+                    }
+                    for (g = 0; g <= 5; g++)
+                    {
+                        partebranca2(aux);
+                        Console.WriteLine(tamanho2);
+                        aux++;
+                    }
+                    for (g = 0; g <= 5; g++)
+                    {
+                        partebranca3(aux);
+                        Console.WriteLine(tamanho3);
+                        aux++;
+                    }
+                    aux = 0;
+                    Console.WriteLine(" ");
+                    i++;
+                }
             }
+
         }
     }
 }
