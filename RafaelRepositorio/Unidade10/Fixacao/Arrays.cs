116745739df79dc4f09a89027784f65a93303cfb
rafael1337 <rafael_aldo_s@hotmail.com>
2014-05-26 11:03:47 -0300

    Adicionado Anos_bisextos complementares

--- RafaelRepositorio/Unidade10/Fixacao/Arrays.cs
+++ RafaelRepositorio/Unidade10/Fixacao/Arrays.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace Unidade10.Fixacao
 {
-    class Arrays
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+    //    class Arrays
+    //    {
+    //        public static int Arrays()
+    //        {
+    //            Random randNum = new Random();
+    //            for(int i = 0; i < 50;i++){           
+    //            int[] Vet = new int[i];
+    //            Vet[i] = randNum.Next(1, 99);       
+    //            }
+    //        }
+    //        static void Main(string[] args)
+    //        {
+    //        }
+    //    }
+    //}
 }
