efaba989fd41d2ef394ec960250aab46ba1aad67
rafael1337 <rafael_aldo_s@hotmail.com>
2014-05-26 08:30:06 -0300

    ExercicioFixacaoConsumoAdicionado

---
+++ RafaelRepositorio/Unidade10/Fixacao/ConsumoDeCombustivel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidade10.ExercicioFixacao
+{
+    class ConsumoDeCombustivel
+    {
+        public static double ConsumoCombustivel(double gasosa, double km)
+        {
+
+            return km /= gasosa; 
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a quantidade de gasolina abastecida: ");
+            double gasosa = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Informe a kilometragem percorrida pelo carro: ");
+            double km = Convert.ToDouble(Console.ReadLine());
+            double result  = ConsumoCombustivel(gasosa,km);
+        }
+    }
+}
+
+
