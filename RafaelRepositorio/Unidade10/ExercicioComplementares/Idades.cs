cde68eab698bc44f9e577795663adeaa2c3d58ac
rafael1337 <rafael_aldo_s@hotmail.com>
2014-06-03 10:26:19 -0300

    Atualizado

--- RafaelRepositorio/Unidade10/ExercicioComplementares/Idades.cs
+++ RafaelRepositorio/Unidade10/ExercicioComplementares/Idades.cs
@@ -1,56 +1,71 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace Unidade10.ExercicioComplementares
-//{
-//    class Idades
-//    {
-//        public static Random Rand = new Random();
-//        public static int Idades(int i, int[] Idade){
-//        int[] Idade = new int[20];
-//        for (i = 0; i <= 20; i++)
-//        {
-//            Idade[i] = Rand.Next(1, 90);
-           
-//        }
-//            return Idade[i];
-//        }
+namespace Unidade10.ExercicioComplementares
+{
+    class Idades
+    {
+        public static Random Rand = new Random();
+        public static int Idades(int i)
+        {
+            int[] idade = new int[20];
+            for (i = 0; i <= 20; i++)
+            {
+                idade[i] = Rand.Next(1, 90);
 
-//        public static int Maior(int maior,int[] Idade)
-//        {
-          
-//            maior = 0;
-//            if (Idade[i] > maior)
-//            {
-//               return maior = Idade[i];
-//            }
-//        }
+            }
+            return idade[i];
+        }
 
-//        public static int Menor(int menor,int[] Idade)
-//        {
-//            menor = 0;
-//            if (Idade[i] > menor)
-//            {
-//               return menor = Idade[i];
-//            }
-//        }
+        public static int Maior(int i, int maior, int[] idade)
+        {
 
-//        public static double Media(double media,int[] Idade)
-//        {
-//            return media += Idade[i] / 20;
-//        }
+            maior = 0;
+            if (idade[i] > maior)
+            {
+                maior = idade[i];
 
-//        public static int Exibir(int[] Idade)
-//        {
-//            Console.WriteLine(Idade[i]);
-//        }
-//        static void Main(string[] args)
-//        {
-//            Idades(int[] Idade);
+            }
+            return maior;
+        }
 
-//        }
-//    }
-//}
+        public static int Menor(int i, int menor, int[] idade)
+        {
+            menor = 0;
+            if (idade[i] > menor)
+            {
+                menor = idade[i];
+            }
+            return menor;
+        }
+
+        public static double Media(int i, int media, int[] idade)
+        {
+            
+            media += idade[i] / 20;
+            return media;
+        }
+
+        public static string Exibir(int[] idade, int i)
+        {
+           return "Idades: " + idade[i];
+        }
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int maior = 0;
+            int[]idade =  new int[20];
+            int menor = 0;
+            int media = 0;
+            Idades(i);
+            Maior(maior,i,idade);
+            Menor(menor,i,idade);
+            Media(media,i,idade);
+            Exibir(idade,i);
+
+        }
+    }
+}
