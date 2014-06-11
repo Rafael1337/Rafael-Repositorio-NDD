e0b465b44f31a3af86c38182c7e46c2de7d200e0
rafael1337 <rafael_aldo_s@hotmail.com>
2014-06-11 09:52:16 -0300

    Atualizado com try catch no program pedido

--- RafaelRepositorio/Pedido/Program.cs
+++ RafaelRepositorio/Pedido/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Pedido
 {
@@ -10,6 +11,37 @@ namespace Pedido
     {
         static void Main(string[] args)
         {
+            string connectionstring = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\aluno\Source\Repos\Rafael-Repositorio-NDD\RafaelRepositorio\Unidade12\BD\RafaelDB.mdf;Integrated Security=True";
+
+            SqlConnection sqlConnection = new SqlConnection(connectionstring);
+
+            sqlConnection.Open();
+
+            //SqlCommand command = new SqlCommand("SELECT * FROM Produto", sqlConnection);
+
+            //SqlDataReader reader = command.ExecuteReader();
+
+        //    while(reader.Read()){
+        //        Console.WriteLine(reader["Nome"]);
+        //    }
+        Console.WriteLine("Digita o nome produto: ");
+        string nome = Console.ReadLine();
+        string splitupdate = String.Format("UPDATE Produto SET Nome = '(0)' WHERE ID = (1)");
+        SqlCommand comand = new SqlCommand(splitupdate,sqlConnection);
+            
+            try
+            {
+                int i = comand.ExecuteNonQuery();
+                Console.WriteLine("Produto atualizado com sucesso! ");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+           
+        
+        
         }
     }
 }
