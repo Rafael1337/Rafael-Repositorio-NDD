3ff0925cfe8062bc75d1b98f40aa85fec0814290
rafael1337 <rafael_aldo_s@hotmail.com>
2014-06-18 09:32:55 -0300

    Complementar12

---
+++ RafaelRepositorio/Unidade12/Complementares/ComandosComplementares.sql
@@ -0,0 +1,8 @@
+﻿SELECT * FROM Funcionario WHERE Salario > 1000 order by PrimeiroNome, SobreNome
+SELECT * FROM Funcionario WHERE Salario > 0 order by Salario
+SELECT * FROM Funcionario WHERE Salario > 0 order by Departamento_Id
+DELETE FROM Funcionario WHERE PrimeiroNome = null
+SELECT * FROM Departamento WHERE Nome = Supervisor
+SELECT * FROM Funcionario WHERE Salario between 500 and 1500 order by PrimeiroNome
+SELECT * FROM Funcionario WHERE PrimeiroNome LIKE 'R%' order by Departamento_Id
+SELECT COUNT(Id) FROM Funcionario
