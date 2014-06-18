a1b5c42718926be68c2528349004c4a8f7fdd9f1
thiagosartor <tt.sartor@hotmail.com>
2014-06-18 10:04:44 -0300

    Corrigir script

--- RafaelRepositorio/Unidade12/Complementares/ComandosComplementares.sql
+++ RafaelRepositorio/Unidade12/Complementares/ComandosComplementares.sql
@@ -2,7 +2,7 @@
 SELECT * FROM Funcionario WHERE Salario > 0 order by Salario
 SELECT * FROM Funcionario WHERE Salario > 0 order by Departamento_Id
 DELETE FROM Funcionario WHERE PrimeiroNome = null
-SELECT * FROM Departamento WHERE Nome = Supervisor
+SELECT D.Nome FROM Departamento as D inner join Funcionario as F on F.Departamento_Id = D.Id and F.Funcao = 'Supervisor'
 SELECT * FROM Funcionario WHERE Salario between 500 and 1500 order by PrimeiroNome
 SELECT * FROM Funcionario WHERE PrimeiroNome LIKE 'R%' order by Departamento_Id
 SELECT COUNT(Id) FROM Funcionario
