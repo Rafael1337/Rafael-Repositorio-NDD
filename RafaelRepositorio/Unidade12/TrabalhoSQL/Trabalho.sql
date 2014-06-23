0c530520aa3f25f7f2763d794b913767aece02b6
rafael1337 <rafael_aldo_s@hotmail.com>
2014-06-23 10:34:10 -0300

    Adicionado TrabalhoSQL

---
+++ RafaelRepositorio/Unidade12/TrabalhoSQL/Trabalho.sql
@@ -0,0 +1,33 @@
+﻿--INSERINDO CURSO 1)
+INSERT INTO TBMatricula (Curso_Id, Aluno_Id) VALUES (4,1)
+
+--2 )
+select nome from AlunoTB where Id > 0 order by Nome;
+select * from TBCurso where id > 0 order by Nome
+
+-- 3) 
+select * from AlunoTB where DataNascimento <= '01/01/1995'
+
+-- 4)
+select count(*) from  AlunoTB
+
+-- 5)
+select count(*) from  AlunoTB
+INNER JOIN TBMatricula
+ON AlunoTB.Id=TBMatricula.Aluno_Id
+INNER JOIN TBMatricula
+ON AlunoTB.Id=TBMatricula.Curso_Id
+
+-- 6)
+select Nome from AlunoTB where DataNascimento <= '01/01/1995'
+
+-- 7)
+select Nome from AlunoTB where Sexo = 'F'
+
+-- 8)
+
+
+-- 9)
+select Nome from TBCurso order by Nome
+
+-- 10)
---
+++ RafaelRepositorio/Unidade12/TrabalhoSQL/TrabalhoTeoricas.sql
@@ -0,0 +1,14 @@
+﻿--1) Defina: SQL. 
+--Linguagem de consulta de banco de dados estruturada, para manipular dados de diferentes tabelas.
+
+--2) Faça um relacionamento cronológico sobre SQL.
+ 
+--3) Liste as principais características de SQL. 
+--Inserir,remover,pesquisar,consultar dados e ter um controle sobre quem os acessa
+
+--4) Descreva a sintaxe do comando SQL: SELECT. Quais cláusulas são obrigatórias e quais são opcionais? 
+--	A clausula obrigatoria do select é o from pra todo select tem que ter um from;
+	
+--5) Qual a importância da linguagem SQL no desenvolvimento de softwares atualmente? 
+--Justifique.
+--ter uma manera de salvar os dados gerados, armazenalos em algum local e ter como recuperalos, e alteralos utilizando o sql
