cbdc5111379c5e44b2579b4e83ef5456d045799f
rafael1337 <rafael_aldo_s@hotmail.com>
2014-06-25 11:31:41 -0300

    Adicionado 1 a 4 unidade complementar

---
+++ RafaelRepositorio/13/Execicios/1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnidadeComplementar.Execicios
+{
+    class _1
+    {
+        public static Random randNum = new Random();
+        public static ArrayList _ArrayList = new ArrayList();
+        static void Main11(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                _ArrayList.Add(randNum.Next(1, 100));                
+            }
+            _ArrayList.Sort();
+            foreach(var item in _ArrayList){                
+                Console.WriteLine(item);
+            }
+
+            _ArrayList.Reverse();
+
+            foreach (var item in _ArrayList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
