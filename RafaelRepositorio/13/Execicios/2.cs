507cc6381aa52fc13886f42868fff75b1ab0e674
rafael1337 <rafael_aldo_s@hotmail.com>
2014-06-26 10:50:56 -0300

    Atualizado

--- RafaelRepositorio/13/Execicios/2.cs
+++ RafaelRepositorio/13/Execicios/2.cs
@@ -1,29 +1,28 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections;
 
 namespace UnidadeComplementar.Execicios
 {
-    class _2
+    internal class _2
     {
         public static Random randNum = new Random();
         public static List<int> _Lista = new List<int>();
-        static void Main123(string[] args)
+
+        private static void Main123(string[] args)
         {
-            
             for (int i = 0; i < 80; i++)
             {
-                _Lista.Add(randNum.Next(1,1000));
-                
+                _Lista.Add(randNum.Next(1, 100));
             }
-            Console.WriteLine("Media: "+ _Lista.Average());
+            Console.WriteLine("Media: " + _Lista.Average());
             Console.WriteLine("Soma de todos: " + _Lista.Sum());
-                Console.WriteLine("Maior valor"+ _Lista.Max());           
-                Console.WriteLine("Menor valor" + _Lista.Min());
-                Console.ReadKey();         
+            Console.WriteLine("Maior valor" + _Lista.Max());
+            Console.WriteLine("Menor valor" + _Lista.Min());
+            Console.ReadKey();
         }
     }
 }
